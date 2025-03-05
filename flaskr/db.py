b2052074c3d677eb38fb70df25e470bf7eb1a809
up2194063 <153841272+up2194063@users.noreply.github.com>
2025-03-05 15:43:15 +0000

    Update db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -25,7 +25,7 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-@click.command('init-db')
+@click.command("init-db")
 def init_db_command():
     init_db()
     click.echo('Initialised the database')
