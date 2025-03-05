8450df730830cd5c3f46bf9cf0714a0154f2932b
up2194063 <153841272+up2194063@users.noreply.github.com>
2025-03-05 16:16:02 +0000

    Fixed syntax error

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -67,7 +67,7 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, email, password) VALUES (?, ?, ?)",
+                    "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
                     (username, email, generate_password_hash(password)),
                 )
                 db.commit()
