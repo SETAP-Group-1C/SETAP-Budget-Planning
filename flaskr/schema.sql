c13b857ed80d177d05e06990ebb5aab1804494ad
up2194063 <153841272+up2194063@users.noreply.github.com>
2025-03-06 17:34:12 +0000

    Added code to delete foreign key relations on re-initialisation

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE payments (
     payment_description TEXT,
     payment_deadline TEXT NOT NULL,
     payment_amount REAL NOT NULL,
-    FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE CASCADE
 );
 
 -- CREATE TABLE FOR TASKS
@@ -45,23 +45,23 @@ CREATE TABLE tasks (
     task_name TEXT NOT NULL,
     task_description TEXT,
     task_deadline TEXT NOT NULL,
-    FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE CASCADE
 );
 
 -- CREATE INTERSECTION TABLE FOR PAYMENTS AND USERS
 CREATE TABLE payments_users (
     payment_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (payment_id) REFERENCES payments (payment_id),
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    FOREIGN KEY (payment_id) REFERENCES payments (payment_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 -- CREATE INTERSECTION TABLE FOR TASKS AND USERS
 CREATE TABLE tasks_users (
     task_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (task_id) REFERENCES tasks (task_id),
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    FOREIGN KEY (task_id) REFERENCES tasks (task_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 -- CREATE INTERSECTION TABLE FOR USERS AND GROUPS
@@ -70,6 +70,6 @@ CREATE TABLE users_groups (
     group_id INTEGER NOT NULL,
     group_creator TEXT NOT NULL
     CHECK (group_creator IN ('Y', 'N')),
-    FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE CASCADE
 );
